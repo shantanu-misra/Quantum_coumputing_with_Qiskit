42798ce5b7aa0f80c420a25972c6e57575756710
yjorKee <64570003+yjorKee@users.noreply.github.com>
2021-04-11 21:02:23 +0530

    Add files via upload

---
+++ OR_with_qubits.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from qiskit import QuantumCircuit, Aer, execute
+
+
+# Define quantum circuit
+qc = QuantumCircuit(3,1)
+
+for input in ['00', '01', '10', '11']:
+
+    # Initialise all qubits to ket 0 to make life easy
+    if input[0] == '1':
+        qc.x(0)
+    if input[1] == '1':
+        qc.x(1)
+
+    qc.cx(0,2)
+    qc.cx(1,2)
+    qc.ccx(0,1,2)
+
+    qc.measure(2,0)
+
+    job = execute(qc,Aer.get_backend('qasm_simulator'),shots=1000)
+    counts = job.result().get_counts(qc)
+    print("Input:", input, "Output:", counts)
